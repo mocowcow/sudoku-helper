6a813eaf8fbf8f3e18bf9512fd36d656a093917e
ariduki <ariduki@gmail.com>
2021-08-21 19:51:43 +0800

    change return value of getAnswer

--- src/SodukuHelper.java
+++ src/SodukuHelper.java
@@ -1,6 +1,18 @@
 
 public class SodukuHelper {
 
+    String[] answer;
+
+    public SodukuHelper() {
+
+    }
+
+    public void solve(char[][] board) {
+        SudokuSolver solver = new SudokuSolver();
+        boolean solvable = solver.solveSudoku(board);
+        
+    }
+
     public static void main(String[] args) {
 
     }
