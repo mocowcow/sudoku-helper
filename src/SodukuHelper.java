9d510dba980bf78da04562591a1326fadcb502f8
ariduki <ariduki@gmail.com>
2021-08-21 20:01:08 +0800

    add methods

--- src/SodukuHelper.java
+++ src/SodukuHelper.java
@@ -2,6 +2,7 @@
 public class SodukuHelper {
 
     String[] answer;
+    int ansIdx;
 
     public SodukuHelper() {
 
@@ -10,7 +11,23 @@ public class SodukuHelper {
     public void solve(char[][] board) {
         SudokuSolver solver = new SudokuSolver();
         boolean solvable = solver.solveSudoku(board);
-        
+        if (solvable) {
+            System.out.println("solved!");
+        } else {
+            System.out.println("the matrix cant be solved!");
+        }
+        answer = solver.getAnwer();
+        ansIdx = 0;
+    }
+
+    //get a hint for next step 
+    public void getHint() {
+        if (ansIdx < answer.length) {
+            System.out.println(answer[ansIdx++]);
+        } else {
+            System.out.println("no hint available!");
+        }
+
     }
 
     public static void main(String[] args) {
